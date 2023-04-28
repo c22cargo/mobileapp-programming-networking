a10cc7c0a1476eaa8461a33e4f907db38ea86095
Carl Gösfeldt <c22cargo@student.his.se>
2023-04-28 18:48:47 +0200

    Added a mountain class. Added a RecycleView which displays the mountains in the MainActivity. Added a RecycleView adapter. Added a layout file "mountain_item.xml" to represent an item in the RecycleView.

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.networking;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @SuppressWarnings("FieldCanBeLocal")
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
@@ -15,6 +21,23 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<Mountain> items = new ArrayList<>(Arrays.asList(
+                new Mountain("Coolt Berg", "Skövde", 1234),
+                new Mountain("Tufft Berg", "Falköping", 2345),
+                new Mountain("Trist Berg", "Hjo", 3456)
+        ));
+
+        RecyclerViewAdapter adapter = new RecyclerViewAdapter(this, items, new RecyclerViewAdapter.OnClickListener() {
+            @Override
+            public void onClick(Mountain item) {
+                Toast.makeText(MainActivity.this, item.getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RecyclerView view = findViewById(R.id.recycler_view);
+        view.setLayoutManager(new LinearLayoutManager(this));
+        view.setAdapter(adapter);
 
         new JsonFile(this, this).execute(JSON_FILE);
     }
---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,26 @@
+package com.example.networking;
+
+public class Mountain {
+    private String name;
+    private String location;
+
+    private int height;
+
+    public Mountain(String name, String location, int height) {
+        this.name = name;
+        this.location = location;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+}
